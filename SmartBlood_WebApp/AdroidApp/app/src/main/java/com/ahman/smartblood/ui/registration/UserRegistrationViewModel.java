0a2b0c024d7e6fa19a4c194d64f8d6cf52b27736
Ahman <33883972+ahmanPg@users.noreply.github.com>
2021-02-17 13:47:59 +0300

    Delete SmartBlood_WebApp directory

--- SmartBlood_WebApp/AdroidApp/app/src/main/java/com/ahman/smartblood/ui/registration/UserRegistrationViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.ahman.smartblood.ui.registration;
-
-import androidx.lifecycle.ViewModel;
-
-public class UserRegistrationViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-
-}
--- SmartBlood_WebApp/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["LOGGED_USER"]);
-unset($_SESSION["USER_ROLE"]);
-session_destroy();
-header("location:index.php");
-
-?>
